b083f9c4350370d069db88faeb2cf36402a02c1b
AaronCorpstein <corps002@morris.umn.edu>
2021-04-21 20:46:33 -0500

    adds config JS file for invoking the REST API

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window._config = {
+    api: {
+        invokeUrl: 'https://y7n4d21u35.execute-api.us-east-1.amazonaws.com/prod/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
