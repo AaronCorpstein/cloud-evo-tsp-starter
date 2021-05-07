4f102cae98ed7f8b23770a243cd07fd67ff172d7
AaronCorpstein <46689469+AaronCorpstein@users.noreply.github.com>
2021-05-06 19:04:22 -0500

    changes the delay of the 'wait5seconds' function to be 10 seconds

--- js/evotsp.js
+++ js/evotsp.js
@@ -220,7 +220,7 @@
       setTimeout(function () {
         console.log(`Done sleeping gen ${generation} at ${Date.now()}`);
         wait_cb(); // Call wait_cb() after the message to "move on" through the waterfall
-      }, 5000);
+      }, 10000);
     }
 
     // Reset a few of the page components that should "start over" at each
